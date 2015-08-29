64272a930a69028c6ddb26d2e6fd8d2c26a6c95c
t0pth4t <t0pth4t@yahoo.com>
2015-08-28 22:50:07 -0500

    moved image links to imgur

--- server/api/library/library.controller.js
+++ server/api/library/library.controller.js
@@ -10,24 +10,49 @@ var _ = require('lodash');
 var libraries = [
   {
     name:'DataTables',
-    src:'https://www.datatables.net/'
+    src:'https://www.datatables.net/',
+    image:'http://i.imgur.com/YUbiO1u.png'
   },
-  {name:'uiTableFilter',
-    src:'https://github.com/gregwebs/jquery-uitablefilter'},
-  {name:'Tablesorter',
-    src:'http://tablesorter.com/docs/'},
-  {name:'Ingrid',
-    src:'http://reconstrukt.com/ingrid/'},
-  {name:'dynatable',
-    src:'http://www.dynatable.com/'},
-  {name:'jqGrid',
-    src:'http://www.trirand.com/'},
-  {name:'slickGrid',
-    src:'https://github.com/mleibman/SlickGrid'},
-  {name:'dhtmlxGrid',
-    src:'http://dhtmlx.com/docs/products/dhtmlxGrid/'},
-  {name:'extjs',
-    src:'https://www.sencha.com/products/extjs/#overview'}
+  {
+    name:'uiTableFilter',
+    src:'https://github.com/gregwebs/jquery-uitablefilter',
+    image:'http://imgur.com/UFDkywb.png'
+  },
+  {
+    name:'Tablesorter',
+    src:'http://tablesorter.com/docs/',
+    image:'http://imgur.com/dvaRhGE.png'
+  },
+  {
+    name:'Ingrid',
+    src:'http://reconstrukt.com/ingrid/',
+    image:'http://imgur.com/7MfNVFs.png'
+  },
+  {
+    name:'dynatable',
+    src:'http://www.dynatable.com/',
+    image:'http://imgur.com/TuxGCeF.png'
+  },
+  {
+    name:'jqGrid',
+    src:'http://www.trirand.com/',
+    image:'http://imgur.com/255NmMf.png'
+  },
+  {
+    name:'slickGrid',
+    src:'https://github.com/mleibman/SlickGrid',
+    image:'http://imgur.com/jdwTdG8.png'
+  },
+  {
+    name:'dhtmlxGrid',
+    src:'http://dhtmlx.com/docs/products/dhtmlxGrid/',
+    image:'http://imgur.com/86iRRm6.png'
+  },
+  {
+    name:'extjs',
+    src:'https://www.sencha.com/products/extjs/#overview',
+    image:'http://imgur.com/UerJT9A.png'
+  }
 ];
 
 // Get list of libraries
@@ -37,11 +62,9 @@ exports.index = function(req, res) {
 
 // Get a single library
 exports.show = function(req, res) {
-
     var library = _.find(libraries, {name:req.params.name});
     if(!library) { return res.status(404).send('Not Found'); }
     return res.json(library);
-
 };
 
 

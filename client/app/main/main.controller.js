e2dece1ad0b5ce57eecb7a61402d0ca4f4602523
t0pth4t <t0pth4t@yahoo.com>
2015-09-02 21:57:56 -0500

    filters working

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,9 +3,29 @@
 angular.module('tablesjsApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
-
+    $scope.catergories = [];
     $http.get('/api/libraries').success(function(libraries) {
+      $scope.orginalThings = libraries;
       $scope.awesomeThings = libraries;
     });
+
+    $scope.addFilter = function(filter){
+      if($scope.catergories.indexOf(filter) === -1){
+        $scope.catergories.push(filter);
+
+      } else{
+        _.pull($scope.catergories, filter);
+      }
+
+    };
+
+    $scope.categoryFilter = function (library) {
+      if(_.every($scope.catergories, function(catergory){
+        return _.contains(library.features, catergory);
+      })){
+        return library;
+      }
+      return;
+    };
 
   });
--- server/api/library/library.controller.js
+++ server/api/library/library.controller.js
@@ -19,48 +19,56 @@ var libraries = [
     name:'uiTableFilter',
     src:'https://github.com/gregwebs/jquery-uitablefilter',
     image:'http://imgur.com/UFDkywb.png',
+    features:['free','sorting','filtering','plugins'],
     description:'small plugin for filtering (hiding) table rows based on their text content'
   },
   {
     name:'Tablesorter',
     src:'http://tablesorter.com/docs/',
     image:'http://imgur.com/dvaRhGE.png',
+    features:['free','sorting','filtering','plugins'],
     description:'tablesorter is a jQuery plugin for turning a standard HTML table with THEAD and TBODY tags into a sortable table without page refreshes. tablesorter can successfully parse and sort many types of data including linked data in a cell. It has many useful features including:'
   },
   {
     name:'Ingrid',
     src:'http://reconstrukt.com/ingrid/',
     image:'http://imgur.com/7MfNVFs.png',
+    features:['free','sorting','filtering','plugins'],
     description:' Ingrid is an unobtrusive jQuery component that adds datagrid behaviors (column resizing, paging, sorting, row and column styling, and more) to your tables.'
   },
   {
     name:'Dynatable',
     src:'http://www.dynatable.com/',
     image:'http://imgur.com/TuxGCeF.png',
+    features:['free','sorting','filtering','plugins'],
     description:'Dynatable is a funner, semantic, interactive table plugin using jQuery, HTML5, and JSON. And it\'s not just for tables.'
   },
   {
     name:'jqGrid',
     src:'http://www.trirand.com/',
     image:'http://imgur.com/255NmMf.png',
+    features:['free','sorting','filtering','plugins'],
     description:'jqGrid is an Ajax-enabled JavaScript control that provides solutions for representing and manipulating tabular data on the web.'
   },
   {
     name:'slickGrid',
     src:'https://github.com/mleibman/SlickGrid',
     image:'http://imgur.com/jdwTdG8.png',
+    features:['free','sorting','filtering','plugins'],
     description:'A lightning fast JavaScript grid/spreadsheet'
   },
   {
     name:'dhtmlxGrid',
     src:'http://dhtmlx.com/docs/products/dhtmlxGrid/',
     image:'http://imgur.com/86iRRm6.png',
+    features:['free','sorting','filtering','plugins'],
     description:'dhtmlxGrid is a full-featured JavaScript grid control that provides cutting-edge functionality, powerful data binding, and fast performance with large data sets.'
   },
   {
     name:'extjs',
     src:'https://www.sencha.com/products/extjs/#overview',
     image:'http://imgur.com/UerJT9A.png',
+    features:['pay','sorting','filtering','plugins'],
     description:''
   }
 ];
